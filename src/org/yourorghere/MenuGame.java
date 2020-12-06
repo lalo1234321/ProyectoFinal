51603346227d756028ff4fd2ec95d68a2798c5b4
lalo1234321 <edominguezc@toluca.tecnm.mx>
2020-12-05 23:55:18 -0600

    First commit

---
+++ src/org/yourorghere/MenuGame.java
@@ -0,0 +1,28 @@
+package org.yourorghere;
+
+import menu.Menu1;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+
+
+
+public class MenuGame  {
+    
+    public static void main(String[] args) {
+        Menu1 menu = new Menu1();
+        menu.setVisible(true);
+        
+        
+        /*JFrame frame = new JFrame();
+      NewJPanel hola = new NewJPanel();
+      frame.add(hola);
+      hola.setVisible(true);
+      frame.setVisible(true);*/
+    }
+
+    
+}
+
+    
+
+
